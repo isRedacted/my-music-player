8d216741360b0e1c9d462f64e419a1182e5b1c5c
isRedacted <farmoffarmers@gmail.com>
2025-04-11 16:32:47 +1000

    Added placeholder qml bridge content

--- app.py
+++ app.py
@@ -6,14 +6,17 @@ from PyQt6.QtQml import QQmlApplicationEngine
 from PyQt6.QtCore import QSettings
 from PyQt6.QtWidgets import QFileDialog, QApplication
 
+from bridge import Bridge
+
 app = QGuiApplication(sys.argv)
 dialogApp = QApplication(sys.argv)
 
 engine = QQmlApplicationEngine()
 
+# Set settings file and check for library path
 path = os.path.dirname(os.path.abspath(__file__))
-
 settings = QSettings(os.path.join(path, "settings.ini"), QSettings.Format.IniFormat)
+
 if not settings.contains("library_dir") or not os.path.exists(settings.value("library_dir")):
     library_dialog = QFileDialog()
     library_dialog.setFileMode(QFileDialog.FileMode.Directory)
@@ -23,7 +26,12 @@ if not settings.contains("library_dir") or not os.path.exists(settings.value("li
         settings.setValue("library_dir", library_dir)
     else:
         sys.exit()
-    
+
+# Bridge between QML frontend and PyQt backend
+bridge = Bridge()
+engine.rootContext().setContextProperty("bridge", bridge)
+
+# START BAYBEEE
 engine.quit.connect(app.quit)
 engine.load('./ui/Main.qml')
 
--- bridge.py
+++ bridge.py
@@ -1,8 +1,19 @@
-from PyQt6 import QObject
+from PyQt6.QtCore import QObject, pyqtSignal, pyqtSlot
 
 # Send/receive play/pause state
 # Send/receive mute state
 # Send/receive song progress state
 
 class Bridge(QObject):
-    print("test")
+    # Signals to QML
+    toQml = pyqtSignal(str)
+    
+    # Init
+    def __init__(self):
+        super().__init__()
+    
+    # Signals from PyQt
+    @pyqtSlot(str)
+    def fromQml(self, msg):
+        print("Receive {msg}")
+        self.toQml.emit("Echo {msg}")
