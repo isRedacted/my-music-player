225d4c6843abb31661ec195eb1c1c4393aa2033b
isRedacted <farmoffarmers@gmail.com>
2025-04-17 11:26:45 +1000

    Reverted changes

--- watcher.py
+++ watcher.py
@@ -1,13 +1,3 @@
 # Watch folder in thread using threading to run it at the same time
 # Maybe add option to not use if scan on startup preferred
 import threading
-
-from watchdog.observers import Observer
-from watchdog.events import FileSystemEventHandler
-
-class LibraryFolderWatcher(FileSystemEventHandler):
-    def __init__(self):
-    
-    def startWatching(self):
-        self.observer.schedule(self.event_handler, self.folder_path, recursive=True)
-        threading.Thread(target=self.observer.start, daemon=True).start()
