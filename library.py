2194cdd02bb6c01b3284441b55abb38fc94a29cd
isRedacted <farmoffarmers@gmail.com>
2025-04-18 23:39:48 +1000

    Placeholder functions

---
+++ library.py
@@ -0,0 +1,8 @@
+from PyQt6.QtCore import QAbstractTableModel
+
+class Library(QAbstractTableModel):
+    def __init__(self):
+        super().__init__()
+        
+    def getLibraryTracks(url):
+        
