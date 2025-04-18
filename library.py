991fe540fabaaf5ddd7961f83ae882ded54adbb7
isRedacted <farmoffarmers@gmail.com>
2025-04-18 23:40:32 +1000

    Added import statement for mutagen and updated comment in getLibraryTracks method

--- library.py
+++ library.py
@@ -1,8 +1,9 @@
 from PyQt6.QtCore import QAbstractTableModel
+import mutagen
 
 class Library(QAbstractTableModel):
     def __init__(self):
         super().__init__()
         
     def getLibraryTracks(url):
-        
+        # Get all tracks with tags at library url
