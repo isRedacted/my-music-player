27e8032c2a75eae11674858e584b6e1c193bf831
isRedacted <farmoffarmers@gmail.com>
2025-04-18 23:13:42 +1000

    Implemented functions

--- playlist.py
+++ playlist.py
@@ -1,17 +1,48 @@
 # Add error checking for missing file
-from PyQt6.QtCore import QAbstractTableModel, QUrl
+import os
+import mutagen
+from PyQt6.QtCore import QUrl, QAbstractTableModel, QModelIndex, Qt
+from charset_normalizer import from_path
 
 class Playlist(QAbstractTableModel):
-    def __init__(self, parent=None):
-        super().__init__(parent)
-        
-    def parseM3UData(self, url):
+    # Playlist is the m3u file url to be processed, columns is a list of user-specified tags to sort the list by (title, artist, etc), and the library directory
+    def __init__(self, playlist, columns, library):
+        super().__init__()
+        self.library = library
+        self.playlist = self.parseM3UData(playlist, library)
+        self.columns = columns
+        self.tracks = self.getPlaylistTags(self.playlist)
+
+    def parseM3UData(self, url, library):
         playlist = []
         qurl = QUrl(url).url()
+        print(str(from_path(url)))
         with open(qurl, 'r') as file:
             for line in file.readlines():
                 if line.startswith("#EXT"):
                     continue
+                elif os.path.exists(line):
+                    playlist.append(line.strip('\n'))
                 else:
-                    playlist.append(line)
+                    playlist.append(library + "/" + line.strip('\n'))
+                    print(line.strip('\n'))
         return playlist
+
+    # Return a list of mutagen FileType objects
+    def getPlaylistTags(self, playlist):
+        tracks = []
+        for t in playlist:
+            tracks.append(mutagen.File(t))
+        return tracks
+
+    # QAbstractTableModel virtual functions
+    def rowCount(self, parent=QModelIndex()):
+        return len(self.tracks)
+
+    def columnCount(self, parent=QModelIndex()):
+        return len(self.columns)
+
+    def data(self, index, role=Qt.ItemDataRole.DisplayRole):
+        row = index.row()
+        column = index.column()
+        return self.tracks[row][column]
