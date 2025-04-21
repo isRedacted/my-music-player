8d7a2a2014095575a2421cbfb0b505fded346cbd
isRedacted <farmoffarmers@gmail.com>
2025-04-22 00:58:54 +1000

    Placeholder stuff

--- library.py
+++ library.py
@@ -7,3 +7,8 @@ class Library(QAbstractTableModel):
         
     def getLibraryTracks(url):
         # Get all tracks with tags at library url
+        library = []
+        try:
+            print("Get file tags with mutagen and add to library")
+        except(mutagen.MutagenError):
+            print("Not a file with valid tags, skipping")
