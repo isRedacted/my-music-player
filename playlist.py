b6c1f4fc90f65bdf0eb0246addbbf6b6923135a0
isRedacted <farmoffarmers@gmail.com>
2025-04-23 15:50:56 +1000

    Filename is in the filething type, removed extraneous listing

--- playlist.py
+++ playlist.py
@@ -2,37 +2,37 @@ from mutagen import MutagenError, File
 from PyQt6.QtCore import QUrl, QAbstractTableModel, QModelIndex, Qt
 
 class Playlist(QAbstractTableModel):
-    def __init__(self, url: QUrl, library: str, columns = ["artist", "title", "album"]):
+    def __init__(self, url, library_dir, columns = ["artist", "title", "album", "year"]):
         """
         `url` is the m3u file url to be processed.
 
         `columns` is a list of user-specified tags to sort the list by (title, artist, etc)
         """
         super().__init__()
-        self.library = library
-        self.track_url_list = self.parseM3UFile(url)
+        self.library_dir = library_dir
+        self.track_url_list = self.parseM3UFile(library_dir + "/" + url)
         self.columns = columns
-        self.tracks = self.getPlaylistTags(self.track_url_list)
-
-    def parseM3UFile(self, url: QUrl):
+        self.tracks = self.getTrackTags(self.track_url_list)
+            
+    def parseM3UFile(self, url: str):
         playlist = []
-        with open(url.toString(), 'r', encoding="utf-8") as file:
+        with open(url, 'r', encoding="utf-8") as file:
             playlist = [
-                self.library + '\\' + line.strip("\n")
+                self.library_dir + '/' + line.strip("\n")
                 for line in file
                 if not line.startswith("#EXT")
             ]
         return playlist
 
-    def getPlaylistTags(self, playlist: list[str]):
+    def getTrackTags(self, playlist: list[str]):
         """Get a list of music file urls and return a dictlist of tags, or a list with a broken link tag, and the url"""
         tracks = []
         for p in playlist:
             try:
                 tags = File(p, easy=True)
-                tracks.append([tags, p])
+                tracks.append(tags)
             except MutagenError:
-                tracks.append(["BROKEN LINK", p])
+                tracks.append(p)
         return tracks
 
     # QAbstractTableModel virtual functions
@@ -44,6 +44,8 @@ class Playlist(QAbstractTableModel):
 
     # TODO
     def data(self, index, role=Qt.ItemDataRole.DisplayRole):
+        #if role == Qt.ItemDataRole.DisplayRole:
+            
         row = index.row()
         column = index.column()
         return self.tracks[row][column]
