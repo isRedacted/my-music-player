1e39b059bee377e6e6b00ed5b8814b925403bc64
isRedacted <farmoffarmers@gmail.com>
2025-04-25 15:38:05 +1000

    Renamed library to library_dir

--- playlist_library.py
+++ playlist_library.py
@@ -1,14 +1,15 @@
 import os
-from PyQt6.QtCore import QAbstractListModel, QUrl
+from PyQt6.QtCore import QAbstractListModel, QModelIndex, Qt
 
 class PlaylistLibrary(QAbstractListModel):
-    def __init__(self, library):
+    def __init__(self, library_dir):
         super().__init__()
-        self.library = library
+        self.library_dir = library_dir
+        self.playlists = self.getFolderPlaylists()
         
     def getFolderPlaylists(self):
         playlists = [str]
-        for dirpath, dirname, filename in os.walk(self.library):
+        for dirpath, dirname, filename in os.walk(self.library_dir):
             for file in filename:
                 if file.endswith(".m3u"):
                     playlists.append(dirpath + "/" + file)
